5724201b2a2a6c65a248e1be6643d8555aff9d50
Masud Rana <masudrana2779@gmail.com>
2021-09-09 17:39:08 +0600

    Product style done

--- components/leftSiteBar/leftSiteBar.tsx
+++ components/leftSiteBar/leftSiteBar.tsx
@@ -1,7 +1,89 @@
 import React from "react";
+import {Accordion} from "react-bootstrap";
+import Link from 'next/link';
+
 const LeftSiteBar = () => {
     return (
-        <div className='leftSiteBarWrap'>Left</div>
+        <div className='leftSiteBarWrap'>
+            <div className="leftSideBarInner">
+                <div className="leftSideBarContent">
+                    <div className="leftSideBar">
+                        <div className="sidebarMenu">
+                            <Accordion>
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>Fruits & Vegetables</Accordion.Header>
+                                    <Accordion.Body>
+                                        <div className="sidebarMenuList">
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Fruits</a>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Vegetables</a>
+                                                </Link>
+                                            </li>
+                                        </div>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>Meat & Fish</Accordion.Header>
+                                    <Accordion.Body>
+                                        <div className="sidebarMenuList">
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Meat</a>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Fish</a>
+                                                </Link>
+                                            </li>
+                                        </div>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="2">
+                                    <Accordion.Header>Fruits & Vegetables</Accordion.Header>
+                                    <Accordion.Body>
+                                        <div className="sidebarMenuList">
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Fruits</a>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Vegetables</a>
+                                                </Link>
+                                            </li>
+                                        </div>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="3">
+                                    <Accordion.Header>Meat & Fish</Accordion.Header>
+                                    <Accordion.Body>
+                                        <div className="sidebarMenuList">
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Meat</a>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link href={'/'}>
+                                                    <a>Fish</a>
+                                                </Link>
+                                            </li>
+                                        </div>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default LeftSiteBar;
--- components/productWrap/productWrap.tsx
+++ components/productWrap/productWrap.tsx
@@ -1,7 +1,53 @@
 import React from "react";
+
 const ProductWrap = () => {
     return (
-        <div className='ProductWrap'>body</div>
+        <div className='productWrap'>
+            <div className="productInner">
+                <div className="productContent">
+                    {[1, 1, 1, 1, 1, 11, 1, 1, , 1].map((item, i) =>
+                        <div className="productSingleItem" key={i}>
+                            <div className="productSingleItemInner">
+                                <div className="productImgContent">
+                                    <div className="productImgInner">
+                                        <div className="productImg">
+                                            <img src="/img/product/RedCherries_zylnoo.jpg" alt="productImg"
+                                                 className='img-fluid'/>
+                                            <span className="offers">10%</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="productInfoContent">
+                                    <div className="productInfoContentInner">
+                                        <h3 className="productTitle">Cherry</h3>
+                                        <span className="productWeight">12 pc(s)</span>
+                                        <div className="productMeta d-flex justify-content-between">
+                                            <div className="productPriceWrapper">
+                                                <span className="productPrice">$1.5 <del>$1.5</del> </span>
+                                            </div>
+                                            <button
+                                                className="btn btn-sm btn-success btnBorder cartBtn d-flex align-items-center">
+                                            <span className="cartIcon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="14.4px" height="12px"
+                                                     viewBox="0 0 14.4 12" fill="currentColor"
+                                                     className="box__Box-sc-5533u7-0 vfnVS">
+                                                    <g data-name="Group 120" transform="translate(-288 -413.89)">
+                                                        <path data-name="Path 154" fill="currentColor"
+                                                              d="M298.7,418.289l-2.906-4.148a.835.835,0,0,0-.528-.251.607.607,0,0,0-.529.251l-2.905,4.148h-3.17a.609.609,0,0,0-.661.625v.191l1.651,5.84a1.336,1.336,0,0,0,1.255.945h8.588a1.261,1.261,0,0,0,1.254-.945l1.651-5.84v-.191a.609.609,0,0,0-.661-.625Zm-5.419,0,1.984-2.767,1.98,2.767Zm1.984,5.024a1.258,1.258,0,1,1,1.319-1.258,1.3,1.3,0,0,1-1.319,1.258Zm0,0"></path>
+                                                    </g>
+                                                </svg>
+                                            </span>
+                                                <span className="cartText">Cart</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
     )
 }
 export default ProductWrap;
